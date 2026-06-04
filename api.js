e705e62c40a8ba7483bdd1ef66bb191efa7af167
Jesus Daniel Gonzalez Bautista <121988244+narvenio@users.noreply.github.com>
2026-06-03 21:33:19 -0400

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 // funcion generica para la api
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://simulador-trading-backend.onrender.com";
 
 export async function apiGet(endpoint) {
     const res = await fetch(`${API_URL}${endpoint}`);
