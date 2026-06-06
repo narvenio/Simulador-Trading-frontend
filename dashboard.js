d76e908c040b2c33997f8ff501574d7d1bd910a7
Jesus Daniel Gonzalez Bautista <121988244+narvenio@users.noreply.github.com>
2026-06-06 12:15:52 -0400

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -223,7 +223,7 @@ async function handleTransaction(type, asset_id, quantity) {
         await loadPortfolio();
         await loadBalance();
         await loadHistoryBalance();
-        await loadTransactions();
+        
     } catch (error) {
         console.error(error);
     }
